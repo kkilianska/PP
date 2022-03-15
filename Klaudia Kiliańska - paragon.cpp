9fc90b5f231ab91081b96e706412fa5fd638e740
kkilianska <Alamakota13>
2022-03-15 17:05:10 +0100

    paragon

---
+++ Klaudia Kiliańska - paragon.cpp
@@ -0,0 +1,24 @@
+//paragon
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    float a,x,p,netto,VAT,brutto;
+    cout << "Podaj cene jednostkowa netto: " << endl;
+    cin >> a;
+    cout << "Podaj liczbe zakupionych sztuk towaru: " << endl;
+    cin>>x;
+    cout << "Podaj wysokosc VAT w procentach: " << endl;
+    cin>>p;
+    //system("pause");
+
+    netto = a * x;
+    VAT = (netto * p)/100;
+    brutto = netto + VAT;
+
+    printf("Wartosc netto: %.1f\n", netto);
+    printf("Wartosc VAT: %.1f\n", VAT);
+    printf("Wartosc brutto: %.1f\n", brutto);
+    return 0;
+}
+
